e5d443e5016c30750aabee7d0c4dedba4e58ff9a
prongbang <prongbang@gmail.com>
2021-06-05 12:52:09 +0700

    add button for handle show dialog

--- app/src/main/java/com/prongbang/smartdialog/MainActivity.kt
+++ app/src/main/java/com/prongbang/smartdialog/MainActivity.kt
@@ -2,6 +2,7 @@ package com.prongbang.smartdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import com.prongbang.dialog.SmartDialogFragment
 import com.prongbang.dialog.SmartIcon
@@ -14,15 +15,17 @@ class MainActivity : AppCompatActivity() {
 	}
 
 	private fun initView() {
-		SmartDialogFragment.Builder(supportFragmentManager)
-				.setIcon(SmartIcon.SUCCESS)
-				.setTitle("SUCCESS")
-				.setMessage("Camera granted, You can access camera to take a picture")
-				.setPositiveButton("CLOSE") {
-					Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT)
-							.show()
-				}
-				.build()
-				.show()
+		findViewById<Button>(R.id.showDialogButton).setOnClickListener {
+			SmartDialogFragment.Builder(supportFragmentManager)
+					.setIcon(SmartIcon.SUCCESS)
+					.setTitle("SUCCESS")
+					.setMessage("Camera granted, You can access camera to take a picture")
+					.setPositiveButton("CLOSE") {
+						Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT)
+								.show()
+					}
+					.build()
+					.show()
+		}
 	}
 }
